c51507e73eec2a3220940531abacbf49f07de2f9
Sergio Herrero <sherrero@gmail.com>
2021-04-11 09:51:53 +0200

    My project is ready

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,3 +1,14 @@
 <?php
 
 include __DIR__ . '/settings/settings.shared.php';
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'admtec00',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'W9UezvL-Z_-6phrSYetB6VTiKfk7aUvctiri4LaErq0ruA-GLoSbvpCuqjTC6ck7ajXObgdBHg';
